4b30fea162162e6be19a57f0d6bbc1f36c6004d4
sakshi5250 <90086693+sakshi5250@users.noreply.github.com>
2022-01-10 19:33:58 +0530

    Update 2_LongestMountain.py

--- Amazon/2_LongestMountain.py
+++ Amazon/2_LongestMountain.py
@@ -1,22 +1,24 @@
-from typing_extensions import Unpack
+def longestMountain(arr):
+    i = 1
+    list = []
+    if len(arr) < 3:
+        return 0
+    if (len(arr) == 3 and arr[0] >= arr[1] and arr[1] <= arr[2]):
+        return 0
+    while i < len(arr):
+        count = 1
+        if arr[i] <= arr[i-1]:
+            i += 1
+        while (i < len(arr) and arr[i] > arr[i-1]):
+            count += 1
+            i += 1
+        if count == 1 or count == len(arr):
+            return 0
+        while (i < len(arr) and arr[i] < arr[i-1]):
+            count += 1
+            i += 1
+        list.append(count)
+    return max(list)
 
 
-def LongestMountain(arr):
-    ret = j = 0
-    n = len(arr)
-    for i in range(n, j+1):
-        j = i
-        Up = down = False
-        while((j+1 < n) and (arr[j+1] > arr[j])):
-            up = True
-            j += 1
-        while(up and (j+1 < n) and (arr[j+1] < arr[j])):
-            down = True
-            j += 1
-        if(up and down):
-            ret = max(j-1+1 < ret)
-            j -= 1
-    return ret
-
-
-print(LongestMountain([2, 1, 4, 7, 3, 2, 5]))
+print(longestMountain([2, 1, 4, 7, 3, 2, 5]))
