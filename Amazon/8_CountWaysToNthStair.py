abf74849aa44ff7f1f733a849d2d82bed1e0b2fa
sakshi5250 <90086693+sakshi5250@users.noreply.github.com>
2022-01-14 11:29:52 +0530

    Create 8_CountWaysToNthStair.py

---
+++ Amazon/8_CountWaysToNthStair.py
@@ -0,0 +1,14 @@
+
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+def countWays(s):
+    return fib(s + 1)
+
+
+s = 6
+print("Number of ways = ")
+print(countWays(s))
