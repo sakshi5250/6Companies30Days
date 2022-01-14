75f126a305e123a1b8e8358f9e94a08f0f386e49
sakshi5250 <90086693+sakshi5250@users.noreply.github.com>
2022-01-14 22:07:27 +0530

    Create 12_ColumnNameFromAGivenColumnNumber.py

---
+++ Amazon/12_ColumnNameFromAGivenColumnNumber.py
@@ -0,0 +1,28 @@
+MAX = 50
+
+
+def printString(n):
+
+    string = ["\0"] * MAX
+
+    i = 0
+
+    while n > 0:
+
+        rem = n % 26
+
+        if rem == 0:
+            string[i] = 'Z'
+            i += 1
+            n = (n / 26) - 1
+        else:
+            string[i] = chr((rem - 1) + ord('A'))
+            i += 1
+            n = n / 26
+    string[i] = '\0'
+
+    string = string[::-1]
+    print("".join(string))
+
+
+printString(26)
