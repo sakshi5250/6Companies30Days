0eafc23b246f9945ecea7c7363aec1ba914791f8
sakshi5250 <90086693+sakshi5250@users.noreply.github.com>
2022-02-02 12:56:16 +0530

    Create Question10.py

---
+++ Walmart/Question10.py
@@ -0,0 +1,23 @@
+
+import random
+
+
+class Solution:
+
+    def __init__(self, radius: float, x_center: float, y_center: float):
+        self.radius = radius
+        self.xc = x_center
+        self.yc = y_center
+
+    def randPoint(self) -> list:
+        while True:
+            rx = (random.random() - 0.5) * 2
+            ry = (random.random() - 0.5) * 2
+
+            if ((rx**2) + (ry**2)) <= 1:
+                break
+
+        return [rx*self.radius+self.xc, ry*self.radius+self.yc]
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(radius, x_center, y_center)
+# param_1 = obj.randPoint()
